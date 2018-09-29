c84756ceb1e0d6b6a0d210b54975035687516d5c
Cody Schafer <dev@codyps.com>
2018-09-28 20:20:35 -0400

    more args

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 #[macro_use]
 extern crate clap;
 
-use clap::{Arg,SubCommand};
+use clap::{Arg,SubCommand,AppSettings};
 
 // hack to try to get `app_from_crate!()` to regenerate.
 #[allow(dead_code)]
@@ -11,11 +11,16 @@ const CARGO_TOML: &'static str = include_str!("../Cargo.toml");
 fn main() {
 
     let matches = app_from_crate!()
+        .arg(Arg::with_name("dry-run")
+             .help("Do not execute anything which would change system state. Print what would state would be changed")
+             .short("N")
+             .global(true)
+            )
+        .setting(AppSettings::SubcommandRequired)
         .subcommand(SubCommand::with_name("zcopy")
             .arg(Arg::with_name("recursive")
-                 .help("Also examine datasets the decend from the specified dataset")
                  .short("R")
-                 .takes_value(false)
+                 .help("Also examine datasets the decend from the specified dataset")
                  )
             .arg(Arg::with_name("SRC_DATASET")
                  .index(1)
@@ -28,13 +33,19 @@ fn main() {
             ).get_matches();
 
 
+    let dry_run = matches.occurrences_of("dry-run") > 0;
+
+
     if let Some(matches) = matches.subcommand_matches("zcopy") {
         let src_dataset = matches.value_of("SRC_DATASET").unwrap();
         let dest_dataset = matches.value_of("DEST_DATASET").unwrap();
 
-        let recursive = matches.occurrences_of("recusrive") > 0;
+        let recursive = matches.occurrences_of("recursive") > 0;
 
-        println!("copy from {} to {} (recursive={})", src_dataset, dest_dataset, recursive); 
+        let dry_run = matches.occurrences_of("dry-run") > 0 || dry_run;
+
+        println!("copy from {} to {} (recursive={})", src_dataset, dest_dataset, recursive);
+        println!("dry_run: {}", dry_run);
 
         // for dataset, find the common base snapshot
         // for each snapshot after the common base
