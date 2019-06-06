d1229b47a2ba736e5ad599b1668545c9f53bf213
Cody P Schafer <dev@codyps.com>
2019-06-06 01:34:57 -0400

    just a few things

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+// bookmark contents:
+//  NAME                                              PROPERTY              VALUE                  SOURCE
+//  innerpool/data/home#znap_2019-06-01-0400_monthly  type                  bookmark               -
+//  innerpool/data/home#znap_2019-06-01-0400_monthly  creation              Sat Jun  1  0:00 2019  -
+//  innerpool/data/home#znap_2019-06-01-0400_monthly  createtxg             8405881                -
+//  innerpool/data/home#znap_2019-06-01-0400_monthly  guid                  8242301612637477726    -
+//
+// guid, createtxg, and creation match the snapshot's properties of the same name
+//
+// snapshot after send|recv has a different:
+//  createtxg 
+//  objsetid
+// and same:
+//  guid
+//  (and everything else)
+
 
 #[macro_use]
 extern crate clap;
@@ -8,6 +24,90 @@ extern crate fmt_extra;
 use zfs_cmd_api::Zfs;
 use clap::{Arg,SubCommand,AppSettings};
 use std::collections::BTreeMap;
+
+#[derive(Debug,PartialEq,Eq,PartialOrd,Ord,Hash,Clone,Debug)]
+enum DatasetType {
+    Snapshot,
+    Bookmark
+}
+
+impl TryFrom<&str> for DatasetType {
+    type Error = String;
+
+    fn try_from(type_str: &str) -> Result<Self, Self::Error> {
+        Ok(match type_str {
+            "bookmark" => DatasetType::Bookmark,
+            "snapshot" => DatasetType::Snapshot,
+            _ => return format!("type {} unrecognized"),
+        })
+    }
+}
+
+#[derive(Debug,PartialEq,Eq,PartialOrd,Ord,Hash,Clone,Debug)]
+struct SubDataset {
+    type_: DatasetType,
+    name: String,
+    createtxg: String,
+}
+
+#[derive(Debug,PartialEq,Eq,PartialOrd,Ord,Hash,Clone,Debug)]
+struct GlobalDataset {
+    guid: String,
+    src: SubDataset,
+    dst: Option<SubDataset>,
+}
+
+#[derive(Debug,PartialEq,Eq,PartialOrd,Ord,Hash,Clone,Debug)]
+struct Dataset {
+    guid: String,
+    ds: SubDataset,
+}
+
+#[derive(Debug,PartialEq,Eq,PartialOrd,Ord,Hash,Clone,Debug)]
+struct CreateTxg {
+    s: String
+}
+
+impl From<&str> for CreateTxg {
+    fn from(s: &str) -> Self {
+        CreateTxg { s: s.to_owned() }
+    }
+}
+
+impl Borrow<str> for CreateTxg {
+    fn borrow(&self) -> &str {
+        self.s
+    }
+}
+
+impl AsRef<str> for CreateTxg {
+    fn as_ref(&self) -> &str {
+        self.s
+    }
+}
+
+#[derive(Debug,PartialEq,Eq,PartialOrd,Ord,Hash,Clone,Debug)]
+struct Guid {
+    s: String
+}
+
+impl From<&str> for Guid {
+    fn from(s: &str) -> Self {
+        Guid { s: s.to_owned() }
+    }
+}
+
+impl Borrow<str> for Guid {
+    fn borrow(&self) -> &str {
+        self.s
+    }
+}
+
+impl AsRef<str> for Guid {
+    fn as_ref(&self) -> &str {
+        self.s
+    }
+}
 
 // hack to try to get `app_from_crate!()` to regenerate.
 #[allow(dead_code)]
@@ -34,7 +134,7 @@ fn main() {
             .arg(Arg::with_name("DEST_DATASET")
                  .index(2)
                  .required(true)
-                )
+         q       )
             ).get_matches();
 
 
@@ -56,71 +156,150 @@ fn main() {
         let dest_zfs = Zfs::from_env_prefix("DEST");
 
         let mut list_builder = zfs_cmd_api::ListBuilder::default();
-        list_builder.include_bookmarks().include_snapshots()
+        list_builder.include_snapshots()
             .depth(1)
-            .with_elements(vec!["createtxg", "name"]);
+            .with_elements(vec!["createtxg", "name", "guid", "type"]);
 
-        let src_list = src_zfs.list_from_builder(list_builder.clone().with_dataset(src_dataset))
+        // - dest will contain some or none of snapshots/bookmarks in source
+        // - dest may have previously recieved but later deleted some snapshots
+        // - src may have previously sent but later deleted some snapshots. Some of these may be
+        //   preserved as bookmarks.
+        //
+        // for each snapshot in source, we want to send it to dest
+        // 
+        // simple algorithm:
+        //   - generate 2 ordered lists of snapshots (excluding bookmarks). order using createtxg
+        //   - use guids to merge the 2 lists into a single list, with 3 markers:
+        //     - in both, only in src, only in dest
+        //   - iterate through the merged list, generating a `send` for each pair of elements,
+        //     excluding those pairs where the second pair exists in both src & dest
+        //
+        // Problems:
+        //  - need to transfer and manage entire list of snapshots. Will this grow too large?
+
+
+        // NOTE: we only get bookmarks for `src` because they aren't useful on the dst
+        //
+        // XXX: we could optimize the zfs-cmd-to-zoop datapassing by not asking for "type" in
+        // dst_list, but it's easier to include it.
+        let src_list =
+            src_zfs.list_from_builder(list_builder.clone().include_bookmarks().with_dataset(src_dataset))
             .expect("src list failed");
         let dst_list = dest_zfs.list_from_builder(list_builder.clone().with_dataset(dest_dataset))
             .expect("dst list failed");
 
 
-        fn to_createtxg_set(list_vecs: Vec<Vec<String>>) -> BTreeMap<String, (String, )>
+        fn to_datasets(list_vecs: Vec<Vec<String>>) ->
+            Vec<Dataset>
         {
-            let mut list_map: BTreeMap<String, (String,)> = BTreeMap::default();
+            let mut dss = Vec::new();
 
             for mut e in list_vecs.into_iter() {
                 let createtxg = e.pop().unwrap();
                 let name = e.pop().unwrap();
-                match list_map.insert(createtxg.clone(), (name.clone(),)) {
-                    Some(x) => println!("duplicate createtxg: {} {} {}", createtxg, name, x.0),
-                    None => {}
-                }
+                let guid = e.pop().unwrap();
+                let type_= e.pop().unwrap();
+
+                let ds = Dataset {
+                    guid: Guid::from(guid),
+                    ds: SubDataset {
+                        type_: DatasetType::try_from(type_),
+                        name: name.to_owned(),
+                        createtxg: CreateTxg::from(createtxg),
+                    }
+                };
+
+                dss.append(ds)
             }
 
-            list_map
+            dss
         }
+
 
         // TODO: determine if dataset has a partial receive, and resume it before proceeding with
         // normal incrimental send
 
-        let src_map = to_createtxg_set(From::from(&src_list));
-        let dst_map = to_createtxg_set(From::from(&dst_list));
-
-        // generate a graph of
-        //      [dst-createtxg|src-createtxg] ->* src-createtxg
-        // in other words:
-        //  - each dst-createtxg serves as a _basis_ for 1 or more src-createtxgs
-        //  - src-createtxgs may in turn be the basis for other src-createtxgs
-        //  - we select a basis (parent) for each src-createtxg by finding the lowest createtxg
-        //    that preceeds it
-        //  - if a dst-createtxg exists which matches a src-createtxg, the src-createtxg is ignored
-        //  - 
-        let mut src_iter = src_map.iter();
-        let mut dst_iter = dst_map.iter();
-        loop {
+        let src_dss = to_datasets(From::from(&src_list));
+        let dst_dss = to_datasets(From::from(&dst_list));
 
 
+        let mut dst_guid_map: BTreeMap<Guid, Dataset> = BTreeMap::default();
+        dst_guid_map.extend(dst_dss.drain());
+
+
+        let mut merged_dss: BTreeMap<CreateTxg, GlobalDataset> = BTreeMap::default();
+
+        // datasets have guids that persist across send/recv. Use this to identify common
+        // elements. guids identify snapshots across pools.
+        // merge sets by guid
+        //
+        // note that bookmarks have a guid equal to the snapshot they were created from
+        //
+        // for the purposes of syncing src to dst, dst datasets that don't exist in any form on src
+        // are irrelevent, so we discard them (they are the items remaining in dst_dss)
+        // 
+        // merged_dss is ordered by (createtxg, guid). We really just want createtxg. Using the
+        // guid lets us avoid having a multimap (a single createtxg may have multiple snaps).
+        //
+        //  XXX: consider the case where multiple snaps exist in the same createtxg. Is it useful
+        //  to have further ordering? Should we include the timestamp here?
+        for src_ds in src_dss.drain() {
+            let dst = dst_guid_map.remove(&src_ds.guid).map(|x| x.ds);
+            let k = (src_ds.createtxg.clone(), src_ds.guid.clone())
+            match merged_dss.insert(k,
+                    GlobalDataset {
+                        guid: src_ds.guid,
+                        src: src_ds.ds,
+                        dst: dst,
+                    }
+                ){
+                Some(x) => {
+                    // continue in a duplicate key case, but warn. This should never happen due to
+                    // our use of the guid as a piece of the key.
+                    eprintln!("WARNING: duplicate key: {?!}", x)
+                },
+                None => {},
+            }
         }
 
+        // iterate over merged_dss
+        //  - send incrimental for each GlobalDataset where `dst` is None and the src type is not a
+        //    bookmark
+        //
+        //  - Use the previous GlobalDataset as the basis for the incremental send
 
-        // Find the most recent createtxg (highest number?) in common between the two
-        // Find all snapshots in src with createtxgs after the common createtxg
-        // Send the found snapshots in order. (`send -I`? may not work with bookmarks).
-        // XXX: consider if we should or can also send bookmarks.
+        
+        let mut prev_dst_ds = None;
+        for ds in merged_dss.drain() {
+
+            match ds.dst {
+                None => {
+                    if ds.src.type_ == DatasetType::Bookmark {
+                        // bookmarks can't be sent, they can only be used as the basis for an
+                        // incirmental send. skip.
+                        continue
+                    }
+
+                    // send it
+                    //
+                    // NEED send|recv (pipe) API in zfs-cmd-api
+
+                    // use as prev after send/recv finishes
+                },
+                Some(x) {
+                    // no transfer needed
+                    // let's just us it as a prev
+                }
+            }
+
+            prev_dst_ds = Some(ds);
+        }
+
 
         // XXX: bookmarks on the SRC allow deletion of snapshots while still keeping send
         // efficiency. As a result, we should create bookmarks to identify points we'll want to
         // sync from
         //
-        // XXX: the createtxg property allows ordering by creation for incremental send/recv
-        // (bookmarks are basically a named createtxg). We may need to use the createtxg in out
-        // common-base-selection
-        //
-        // XXX: datasets have guids that persist across send/recv. Use this to identify common
-        // elements. guids identify snapshots across pools.
-
         // for dataset, find the common base snapshot
         // for each snapshot after the common base
         //  send it snap
